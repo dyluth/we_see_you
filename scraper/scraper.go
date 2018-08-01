c0194b534409bb5526e9196f8dedc69efe5edf51
cam <cam@larptronics.com>
2018-08-01 19:26:17 +0100

    scraper works for people

--- scraper/scraper.go
+++ scraper/scraper.go
@@ -1,21 +1,30 @@
-// find_links_in_page.go
 package scraper
 
 import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
+// ElementProcesser is the interface for how we process pages.
 type ElementProcesser interface {
 	ProcessElement(index int, element *goquery.Selection)
 }
 
-func scrape(ep ElementProcesser) error {
+// Relevent info for a person to look up in theyworkforyou
+type Person struct {
+	ID           string
+	Name         string
+	Constituency string
+}
+
+func scrape(ep ElementProcesser, person Person) error {
 	// Make HTTP request
-	response, err := http.Get("https://www.theyworkforyou.com/mp/24901/steve_brine/winchester/votes")
+	urlString := fmt.Sprintf("https://www.theyworkforyou.com/mp/%v/%v/%v/votes", person.ID, person.Name, person.Constituency)
+	response, err := http.Get(urlString)
 	if err != nil {
 		return err
 	}
@@ -38,25 +47,14 @@ func scrape(ep ElementProcesser) error {
 	return nil
 }
 
-// ScrapeVotingRecord looks at the specific person, and pulls out their voting summaries for each topic
-func ScrapeVotingRecord(name string) (map[string]string, error) {
-
-	ep := ElementProcesserImpl{
-		results: make(map[string]string),
-	}
-	scrape(&ep)
-
-	return nil, nil
-
-}
-
+// ElementProcesserImpl processes the pages to get the good info out of it.
 type ElementProcesserImpl struct {
 	results map[string]string
 }
 
-// This will get called for each HTML element found
+// ProcessElement will get called for each `ul` element found
 func (ep *ElementProcesserImpl) ProcessElement(index int, element *goquery.Selection) {
-	// See if the href attribute exists on the element
+	// we are only interested in the vote-descriptions ones
 	isVD := element.HasClass("vote-descriptions")
 	fmt.Printf("found %v: good? %v \n", index, isVD) //, element.Text())
 	if isVD {
@@ -64,8 +62,29 @@ func (ep *ElementProcesserImpl) ProcessElement(index int, element *goquery.Selec
 	}
 }
 
+// ProcessChildElement is the bit that looks at the voting section and pulls out the useful information
 func (ep *ElementProcesserImpl) ProcessChildElement(index int, element *goquery.Selection) {
 	fmt.Printf("VoteDesc: %v, %v\n", index, element.Text())
+	// get all the text before the `Show votes` bit
+	split := strings.Split(element.Text(), "Show votes")
+	if len(split) != 2 {
+		fmt.Printf("ERROR! splitting text didnt split on `Show votes` into 2 bits but `%v`\n", len(split))
+		return
+	}
+	voteSummary := strings.TrimSpace(split[0])
 
-	// TODO get the key and the result and put it into ep.results
+	// we build sectionname out of the children parts
+	sectionName := ""
+	element.Children().Each(func(index int, item *goquery.Selection) {
+		text := strings.TrimSpace(item.Text())
+		if index == 0 {
+			sectionName = text
+		} else if index == 1 {
+			if !strings.Contains(text, "Show votes") {
+				// if there are multiple parts before `show votes` append them together
+				sectionName = fmt.Sprintf("%v %v", sectionName, text)
+			}
+		}
+	})
+	ep.results[sectionName] = voteSummary
 }
--- scraper/scraper_test.go
+++ scraper/scraper_test.go
@@ -15,8 +15,15 @@ var (
 
 func TestScraper(t *testing.T) {
 
-	ep := *testElementProcesserImpl{}
-	scrape(ep)
+	ep := testElementProcesserImpl{}
+	p := Person{
+		ID:           "24901",
+		Name:         "steve_brine",
+		Constituency: "winchester",
+	}
+	err := scrape(&ep, p)
+
+	require.NoError(t, err)
 
 	for _, t := range sections {
 		fmt.Printf(" sections[\"%v\"]=[]string{}\n", t)
@@ -32,7 +39,7 @@ type testElementProcesserImpl struct {
 }
 
 // processVotePrintout - just a test func to print out the results
-func (tep *testElementProcesserImpl) processVotePrintout(index int, element *goquery.Selection) {
+func (tep *testElementProcesserImpl) ProcessElement(index int, element *goquery.Selection) {
 	fmt.Printf("VoteDesc: %v, %v\n", index, element.Text())
 	element.Children().Each(processVoteChildrenPrintout)
 }
@@ -47,4 +54,25 @@ func processVoteChildrenPrintout(index int, element *goquery.Selection) {
 			sections[len(sections)-1] = fmt.Sprintf("%v %v", sections[len(sections)-1], text)
 		}
 	}
+}
+
+// TestScraperReal is how we should use the scraper to pull out someones record
+func TestScraperReal(t *testing.T) {
+	ep := ElementProcesserImpl{results: make(map[string]string)}
+	p := Person{
+		ID:           "24901",
+		Name:         "steve_brine",
+		Constituency: "winchester",
+	}
+	err := scrape(&ep, p)
+
+	require.NoError(t, err)
+
+	fmt.Println("==== RESULTS ====")
+	for k, v := range ep.results {
+		fmt.Printf("  [%v]:[%v]\n", k, v)
+	}
+	require.True(t, len(ep.results) > 0, "we should have found some results!")
+
+	require.True(t, false, "killed")
 }
